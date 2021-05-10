f1ca846973a564dae7a7df070536ac3ee0a0878d
Jenny Russell <moosyoolka@gmail.com>
2021-05-10 15:57:45 -0700

    refactored quiz, added variable for user response

--- quiz.js
+++ quiz.js
@@ -5,106 +5,84 @@ let answerList = document.getElementById('list');
 let listItem = document.getElementById("classItem");
 
 
-let typeAnswer;
+let currentSelection = [];
 let genreAnswer = [];
 
 
 let quiz = {
     questions: [{
             question: "What type of anime do you enjoy watching?",
-            options: ["Shounen", "Shoujo", "Any"],
-            selectedAnswer: typeAnswer
+            options: ["Shounen", "Shoujo"],
         },
         {
             question: "What is your preferred anime genre?",
             options: ["Action", "Horror", "Fantasy", "Comedy", "Drama", "Psychological", "Romance", "Mystery"],
-            selectedAnswer: genreAnswer
-        },
-        {
-            question: "What type of anime do you enjoy watching?",
-            options: ["Shounen", "Shoujo", "Any"],
-            selectedAnswer: typeAnswer
         },
     ],
 }
 
 
 
-i = 0;
+let i = 0;
 optionsArray = quiz.questions[i].options;
 questionsArray = quiz.questions[i].question;
 let q = questionContainer;
 let a = answerList;
 
 function renderQuestion() {
-
-    q.innerHTML = "<p>" + quiz.questions[i].question + "</p";
-
-    optionsArray.forEach(addListItem);
-    console.log(optionsArray);
+    let currentOptions = quiz.questions[i].options;
+    q.innerHTML = "<p>" + quiz.questions[i].question + "</p>";
+    a.innerHTML = "";
+    currentOptions.forEach(addListItem);
+    console.log(currentOptions);
 }
 
 function addListItem(item) {
-    document.getElementById(a.innerHTML += `<li id="classItem">${item}</li>`);
-
-    selectChoices();
-}
-
-
-
-// q.innerHTML = quiz.questions[current].question;
-// a.innerHTML = quiz.questions[current].options;
-
-
-function next() {
-    removeOldListItems();
-    let current = 0;
-    let questionIndex = current + 1;
-
-    if (current < quiz.questions.length - 1) {
-        current++;
-        questionIndex = current + 1;
-        q.innerHTML = quiz.questions[current].question;
-    }
-    if (current < quiz.questions[current].options.length - 1) {
-
-        let opArray = quiz.questions[current].options;
-        opArray.forEach(addNextItem);
-
-        function addNextItem(item) {
-
-
-            document.getElementById(a.innerHTML += `<li id="classItem">${item}</li>`);
-        }
-
-    }
-
+    a.innerHTML += `<li class="classItem" onclick="selectTest('${item}')">${item}</li>`;
 
 
 }
 
-function removeOldListItems() {
-    let e = document.querySelector("ul");
-    let child = e.lastElementChild;
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
+
+
+
+
+// function selectChoices() {
+//     document.querySelector('#list')
+//         .addEventListener('click', event => {
+//             let target = event.target;
+//             target.setAttribute("style", "background-color: lightblue");
+//             if (target.matches('li')) {
+//                 let value = target.innerHTML
+//                 console.log(value);
+//             }
+//         });
+
+// };
+
+function selectTest(value) {
+    console.log(value);
+    if (currentSelection.includes(value) === false) {
+        currentSelection.push(value)
+
     }
 }
 
-function selectChoices() {
-    document.querySelector('#list')
-        .addEventListener('click', event => {
-            let target = event.target;
-            if (target.matches('li')) {
-                let value = target.innerHTML
-                console.log(value);
-            }
-        });
+function submitTest() {
+    genreAnswer = genreAnswer.concat(currentSelection)
+    currentSelection = [];
+    if (i === quiz.questions.length - 1) {
+        endQuiz();
+    } else {
+        i++;
+        console.log(genreAnswer);
+        renderQuestion();
+    }
+}
 
-};
+function endQuiz() {
+    document.getElementById("quiz-box").innerHTML = "<h3>" + genreAnswer + "</h3>"
+}
 
-
-
-
+console.log(quiz.questions.length);
 renderQuestion();
