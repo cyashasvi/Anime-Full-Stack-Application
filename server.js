97fbace9f70c96e933a4e3cc2651cd52efab990e
Jeremy <jeremyainsworth0791@gmail.com>
2021-05-10 19:55:52 -0700

    wip front end connections to back end and withauth added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 
 // const routes = require('./controllers');
 
