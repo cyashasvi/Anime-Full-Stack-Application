89265762fad9fd22464b09c535add8735a28cda1
Jenny Russell <moosyoolka@gmail.com>
2021-05-12 21:46:52 -0700

    fixes

--- controllers/api/preferenceRoutes.js
+++ controllers/api/preferenceRoutes.js
@@ -33,41 +33,6 @@ router.get('/preferences/:id', async(req, res) => {
     }
 });
 
-const router = require('express').Router();
-const { Preferences } = require('../../models');
-
-router.get('/preferences/:id', async(req, res) => {
-    try {
-        const prefData = await Preferences.findByPk(req.params.id, {
-            include: [{
-                model: Preferences,
-                attributes: [
-                    'id',
-                    'preferredGenre',
-                    'genre_id',
-                ],
-            }, ],
-        });
-
-        const preferences = prefData.get({ plain: true });
-        res.render('preferences', {
-            preferences,
-            countVisit: req.session.countVisit,
-        });
-
-        req.session.save(() => {
-            if (req.session.countVisit) {
-                req.session.countVisit++;
-            } else {
-                req.session.countVisit = 1;
-            };
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-
 
 router.post('/', async(req, res) => {
     const data = req.body.preferredGenre.join();
