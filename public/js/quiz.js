c7d990c120d2e648e4ecc9ee5263c2941856c8b8
Jenny Russell <moosyoolka@gmail.com>
2021-05-12 21:39:10 -0700

    added post to click functionality on quiz

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -2,8 +2,9 @@ let nextBtn = document.getElementById('nextBtn');
 let questionContainer = document.getElementById('question-container');
 let answerList = document.getElementById('list');
 let listItem = document.getElementById("classItem");
+const confirmBtn = document.getElementById("confirmBtn");
 let currentSelection = [];
-let genreAnswer = [];
+let preferredGenre = [];
 let quiz = {
     questions: [{
             question: "What type of anime do you enjoy watching?",
@@ -49,18 +50,46 @@ function selectTest(value) {
 }
 
 function submitTest() {
-    genreAnswer = genreAnswer.concat(currentSelection)
+    preferredGenre = preferredGenre.concat(currentSelection)
     currentSelection = [];
     if (i === quiz.questions.length - 1) {
         endQuiz();
     } else {
         i++;
-        console.log(genreAnswer);
+        console.log(preferredGenre);
         renderQuestion();
     }
 }
 
 function endQuiz() {
-    document.getElementById("quiz-box").innerHTML = "<h3>" + genreAnswer + "</h3>"
+    document.getElementById("quiz-box").innerHTML = "<h3>" + preferredGenre.join(' ') + "</h3>" +
+        "<button class='p-2 rounded text-white bg-blue-400 w-24 my-2' id='confirmBtn' type='button' onclick='viewResults()'>" + "Confirm" + "</button>"
+
 }
+
+const postToPreferences = async(e) => {
+    console.log(preferredGenre);
+
+    const response = await fetch('/api/preferences', {
+        method: 'POST',
+        body: JSON.stringify({ preferredGenre }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('failure to post');
+    }
+}
+
+
+function viewResults() {
+    console.log("click works");
+    postToPreferences();
+
+}
+
+
+
 renderQuestion();
