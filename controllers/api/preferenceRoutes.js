e7dbbfe27f78239270289960d07f4d487164501f
Jenny Russell <moosyoolka@gmail.com>
2021-05-12 21:41:21 -0700

    added preferences post route

--- controllers/api/preferenceRoutes.js
+++ controllers/api/preferenceRoutes.js
@@ -33,6 +33,58 @@ router.get('/preferences/:id', async(req, res) => {
     }
 });
 
+const router = require('express').Router();
+const { Preferences } = require('../../models');
 
+router.get('/preferences/:id', async(req, res) => {
+    try {
+        const prefData = await Preferences.findByPk(req.params.id, {
+            include: [{
+                model: Preferences,
+                attributes: [
+                    'id',
+                    'preferredGenre',
+                    'genre_id',
+                ],
+            }, ],
+        });
+
+        const preferences = prefData.get({ plain: true });
+        res.render('preferences', {
+            preferences,
+            countVisit: req.session.countVisit,
+        });
+
+        req.session.save(() => {
+            if (req.session.countVisit) {
+                req.session.countVisit++;
+            } else {
+                req.session.countVisit = 1;
+            };
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+router.post('/', async(req, res) => {
+    const data = req.body.preferredGenre.join();
+    const obj = {
+        preferredGenre: data,
+        user_id: req.session.user_id
+    }
+    console.log(obj)
+    try {
+        const prefData = await Preferences.create(obj);
+        console.log(prefData);
+        res.status(200).json(prefData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
 
 module.exports = router;
