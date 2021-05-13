8d8c6a9ed785067fc810da0b166164e235408dd6
cyashasvi <cyashasvi@gmail.com>
2021-05-12 20:18:48 -0700

    made animeroutes work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const preferenceRoutes = require('./preferenceRoutes');
 const usersRoute = require('./usersRoute');
+const animeRoutes = require('./animeRoutes')
 
 
 router.use('/users', usersRoute);
 router.use('/preferences', preferenceRoutes);
+router.use('/anime', animeRoutes);
 
 module.exports = router;
