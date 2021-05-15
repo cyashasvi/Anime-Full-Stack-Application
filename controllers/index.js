e1ba86972596ae2ad8bd2262de02cf90b6b6296d
cyashasvi <cyashasvi@gmail.com>
2021-05-15 14:07:07 -0700

    added anime.js file and changes to homeroutes for a new get route

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,12 @@ const router = require('express').Router();
 // const animeRoutes = require('./api/animeRoutes');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js')
+const animePage = require('./api/animePage')
 
 // const preferenceRoutes = require('./api/preferenceRoutes');
 
 router.use('/', homeRoutes);    // serve the pages, localhost:300/signup
 router.use('/api', apiRoutes); //serve the data, localhost:300/api/anime /998383
 // router.use('/preferences', preferenceRoutes);
-
+router.use('/animePage', animePage)
 module.exports = router;
